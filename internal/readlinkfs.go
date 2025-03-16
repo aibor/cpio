4d3826af7d7723fadbfd579700b3bb3e43194ed6
Tobias Böhm <code@aibor.de>
2025-03-16 20:48:14 +0100

    style: use conventional import alias

--- internal/readlinkfs.go
+++ internal/readlinkfs.go
@@ -2,26 +2,26 @@ package internal
 
 import (
 	"errors"
-	iofs "io/fs"
+	gofs "io/fs"
 	"os"
 )
 
-// Replace with [iofs.ReadLinkFS] once available (planned for 1.25). See
+// Replace with [gofs.ReadLinkFS] once available (planned for 1.25). See
 // https://github.com/golang/go/issues/49580
 type ReadLinkFS interface {
-	iofs.FS
+	gofs.FS
 
 	ReadLink(name string) (string, error)
-	Lstat(name string) (iofs.FileInfo, error)
+	Lstat(name string) (gofs.FileInfo, error)
 }
 
-func ReadLink(fsys iofs.FS, name string) (string, error) {
+func ReadLink(fsys gofs.FS, name string) (string, error) {
 	rlFS, ok := fsys.(ReadLinkFS)
 	if !ok {
-		return "", &iofs.PathError{
+		return "", &gofs.PathError{
 			Op:   "readlink",
 			Path: name,
-			Err:  iofs.ErrInvalid,
+			Err:  gofs.ErrInvalid,
 		}
 	}
 
@@ -31,27 +31,27 @@ func ReadLink(fsys iofs.FS, name string) (string, error) {
 var _ ReadLinkFS = (*readLinkFS)(nil)
 
 type readLinkFS struct {
-	iofs.FS
+	gofs.FS
 	readLinkFn func(name string) (string, error)
-	lstatFn    func(name string) (iofs.FileInfo, error)
+	lstatFn    func(name string) (gofs.FileInfo, error)
 }
 
 func (fsys *readLinkFS) ReadLink(name string) (string, error) {
 	return fsys.readLinkFn(name)
 }
 
-func (fsys *readLinkFS) Lstat(name string) (iofs.FileInfo, error) {
+func (fsys *readLinkFS) Lstat(name string) (gofs.FileInfo, error) {
 	return fsys.lstatFn(name)
 }
 
 // Replace with [os.DirFS] once available (planned for 1.25). See
 // https://github.com/golang/go/issues/49580
-func ReadLinkDirFS(dir string) iofs.FS {
+func ReadLinkDirFS(dir string) gofs.FS {
 	join := func(name string) (string, error) {
 		if dir == "" {
 			return "", errors.New("os: DirFS with empty root")
 		}
-		if !iofs.ValidPath(name) {
+		if !gofs.ValidPath(name) {
 			return "", os.ErrInvalid
 		}
 		if os.IsPathSeparator(dir[len(dir)-1]) {
@@ -69,7 +69,7 @@ func ReadLinkDirFS(dir string) iofs.FS {
 			}
 			return os.Readlink(path)
 		},
-		lstatFn: func(name string) (iofs.FileInfo, error) {
+		lstatFn: func(name string) (gofs.FileInfo, error) {
 			path, err := join(name)
 			if err != nil {
 				return nil, err
